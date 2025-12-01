cac465fe799b9e332f87184dcf4d5e5ea0365524
B222270821 <b222270821@must.edu.mn>
2025-12-01 23:27:05 +0800

    jijig zasvar

--- lab04-/back-end/src/db.js
+++ lab04-/back-end/src/db.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/placesApp");
-    console.log("✅ MongoDB холбогдлоо");
+    console.log("MongoDB холбогдлоо");
   } catch (err) {
-    console.error("❌ MongoDB холболтын алдаа:", err);
+    console.error("MongoDB холболтын алдаа:", err);
     process.exit(1);
   }
 }
